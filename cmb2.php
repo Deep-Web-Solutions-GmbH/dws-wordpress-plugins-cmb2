6e1d5c583c9da74707fa3ca9c552eb04de11969d
Fatine Tazi <f.tazi@deep-web-solutions.de>
2019-11-28 11:26:37 +0100

    Comitting unversioned files

---
+++ cmb2.php
@@ -0,0 +1,59 @@
+<?php
+
+namespace Deep_Web_Solutions\Plugins;
+use Deep_Web_Solutions\Base\DWS_Functionality_Template;
+
+if (!defined('ABSPATH')) { exit; }
+
+/**
+ * Adapter for the ACF Pro plugin.
+ *
+ * @since   2.0.0
+ * @version 2.0.0
+ * @author  Fatine Tazi <f.tazi@deep-web-solutions.de>
+ *
+ * @wordpress-plugin
+ * Plugin Name:         DeepWebSolutions CMB2 Compatibility
+ * Description:         This plugin handles all the core custom extensions to the 'CMB2' plugin.
+ * Version:             2.0.0
+ * Author:              Deep Web Solutions GmbH
+ * Author URI:          https://www.deep-web-solutions.de
+ * License:             GPL-3.0+
+ * License URI:         http://www.gnu.org/licenses/gpl-3.0.txt
+ * Text Domain:         dws_custom-extensions_dh8gfh38g7hr38gd
+ * Domain Path:         /languages
+ */
+final class CMB2_Compatibility extends DWS_Functionality_Template {
+    //region INHERITED FUNCTIONS
+
+    /**
+     * @since   2.0.0
+     * @version 2.0.0
+     *
+     * @see     DWS_Functionality_Template::are_prerequisites_fulfilled()
+     *
+     * @return  bool
+     */
+    protected static function are_prerequisites_fulfilled() {
+        return is_plugin_active('cmb2/init.php');
+    }
+
+    /**
+     * @since   2.0.0
+     * @version 2.0.0
+     *
+     * @see     DWS_Functionality_Template::load_dependencies()
+     */
+    protected function load_dependencies() {
+        /** @noinspection PhpIncludeInspection */
+        /** Force load ACF at this point in time ... */
+        require_once(WP_PLUGIN_DIR . '/cmb2/init.php');
+
+        /** @noinspection PhpIncludeInspection */
+        /** The CMB2 Adapter. */
+        require_once(self::get_includes_base_path() . 'class-cmb2-settings-adapter.php');
+        CMB2\DWS_CMB2_Adapter::maybe_initialize_singleton('gm8ugh874hngf87gbcu', true, self::get_root_id());
+    }
+
+    //endregion
+} CMB2_Compatibility::maybe_initialize_singleton('fny47ytvihnvcjvn8nuhyce8', true);
---
+++ readme.txt
@@ -0,0 +1,13 @@
+=== DWS WordPress CMB2 Plugin ===
+Contributors: Fatine Tazi, Antonius Hegyes
+Requires at least: 5.3
+Tested up to: 5.3
+Requires PHP: 7.3
+Stable tag: 2.0.0
+License: GPLv3 or later
+License URI: http://www.gnu.org/licenses/gpl-3.0.html
+
+=== Changelog ===
+
+= 2.0.0 =
+* First official release. Marks compatibility with DWS Core 2.0.0
